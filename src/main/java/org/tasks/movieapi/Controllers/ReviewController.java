0584684ce9ec40ce2de7c6870dfa1430d07656b0
YousefFayez20 <96171490+YousefFayez20@users.noreply.github.com>
2025-09-13 16:09:32 +0300

    updated controllers

--- src/main/java/org/tasks/movieapi/Controllers/ReviewController.java
+++ src/main/java/org/tasks/movieapi/Controllers/ReviewController.java
@@ -4,10 +4,7 @@ package org.tasks.movieapi.Controllers;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import org.tasks.movieapi.Models.Review;
 import org.tasks.movieapi.Services.ReviewService;
 
@@ -15,6 +12,7 @@ import java.util.Map;
 
 @RestController
 @RequestMapping("/api/v1/reviews")
+@CrossOrigin(origins = "*")
 public class ReviewController {
     @Autowired
     private ReviewService service;
